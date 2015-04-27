ace5f8ae713c947a1fe655642033179b3a93af6d
Selina Ross <ross.selina@gmail.com>
2015-04-27 12:03:46 -0600

    php includes for header/footer

--- php_class_assignment/index.php
+++ php_class_assignment/index.php
@@ -2,7 +2,7 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>Full Page</title>
+    <title>Full Page Re-creation</title>
     <link href='http://fonts.googleapis.com/css?family=Cabin:400,500,600,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="styles/styles.css">
 </head>
