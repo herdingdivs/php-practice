e6652f6d776430ecb7deb43fc6ea7c2336e9191d
Selina Ross <ross.selina@gmail.com>
2015-04-27 12:15:06 -0600

    a simple form with name input and welcome message

--- php_class_assignment_1/index.php
+++ php_class_assignment_1/index.php
@@ -4,7 +4,7 @@
         <form action="welcome.php" method="post">
             <legend>Enter your name:</legend>
             Name: <input type="text" name="name"<br>
-            <input type="submit" name="Submit" value="Submit" />
+            <input type="submit" name="Submit" value="Submit">
         </form>
      </fieldset>
 </body>
